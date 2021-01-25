d6e736da40277e202f671cd0b4b83daeccd26093
helene <h.spring@tudelft.nl>
2021-01-25 14:28:38 +0100

    Add docstring to area calculating function

--- test.py
+++ test.py
@@ -4,4 +4,10 @@ def surface_circle(radius):
     return np.pi*radius**2
 
 def area_circle(radius):
+    """
+    This function calculates the area of a circle of a given radius
+    
+    :param float radius: the radius of the circle
+    
+    """
     return np.pi*radius**2
